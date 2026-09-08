50973f81de63bf560203dfc80d25ea3da45f0006
Соня <buddson@yandex.ru>
2026-09-08 16:07:53 +0300

    subtitle_extractor commit

--- src/scene_detector.py
+++ src/scene_detector.py
@@ -1,17 +1,19 @@
 import os
 from scenedetect import detect, ContentDetector
 import pandas as pd
-path = "C:/Users/Соня/PycharmProjects/PythonProject/VideoContentRanking/data/raw/demolition.mp4"
-scene_data = []
-scenes = detect(path, ContentDetector())
-for i, (scene_start, scene_end) in enumerate(scenes,1):
-    scene_data.append({
-        'path_name': os.path.basename(path),
-        'scene_id': i,
-        'start_time': scene_start,
-        'end_time': scene_end
+def detect_scenes(video_path):
+    path = video_path
+    scene_data = []
+    scenes = detect(path, ContentDetector())
+    for i, (scene_start, scene_end) in enumerate(scenes,1):
+        scene_data.append({
+            'path_name': os.path.basename(path),
+            'scene_id': i,
+            'start_time': scene_start,
+            'end_time': scene_end
 
-    })
-df = pd.DataFrame(scene_data)
-df.to_csv('C:/Users/Соня/PycharmProjects/PythonProject/VideoContentRanking/data/annotations/scenes_timestamp.csv', index=False)
-print(f"Saved {len(scenes)} scenes to scenes_timestamps.csv")
+        })
+    df = pd.DataFrame(scene_data)
+    df.to_csv('C:/Users/Соня/PycharmProjects/PythonProject/VideoContentRanking/data/annotations/scenes_timestamp.csv', index=False)
+    print(f"Saved {len(scenes)} scenes to scenes_timestamps.csv")
+    return scenes
