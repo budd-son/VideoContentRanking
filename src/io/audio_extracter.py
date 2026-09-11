bf6c153249540bd66b0b0f4538e1351ab53993e5
Соня <buddson@yandex.ru>
2026-09-12 02:08:14 +0300

    audio extracter refact

--- src/io/audio_extracter.py
+++ src/io/audio_extracter.py
@@ -1,18 +1,23 @@
 import os
+from pathlib import Path
+
 import ffmpeg
+import logging
+from typing import Optional
 
+logger = logging.getLogger(__name__)
 
-def extract_audio(video_path):
+def extract_audio(video_path: str, out_dir: str) -> Optional[str]:
+    Path(out_dir).mkdir(parents=True, exist_ok=True)
+    base = Path(video_path).stem
+    wav_path = str(Path(video_path)/f"{base}_full_audio.wav")
 
-    output_dir = "/data/audioVideoContent"
-    os.makedirs(output_dir, exist_ok=True)
-    base_name = os.path.basename(video_path).split('.')[0]
-    wav_path = os.path.join(output_dir, f"{base_name}_full_audio.wav")
     try:
         stream = ffmpeg.input(video_path)
         output = ffmpeg.output(stream.audio, wav_path, format="wav", acodec="pcm_s16le")
-        ffmpeg.run(output)
+        ffmpeg.run(output, capture_stdout=True, capture_stderr=True)
+        logger.info("extract audio... to %s", wav_path)
         return wav_path
     except Exception as e:
-        print(f"Ошбика при извлечении: {e}")
+        logger.exception(f"Ошбика при извлечении: {e}")
         return None
