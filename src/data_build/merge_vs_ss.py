7089fe8f4652be7cf18741c0db17504b6a0866cf
Соня <buddson@yandex.ru>
2026-09-26 18:48:01 +0300

    fix cur_per_sec commit

--- src/data_build/merge_vs_ss.py
+++ src/data_build/merge_vs_ss.py
@@ -23,7 +23,9 @@ def build_scene_data(visual_scenes, speech_scenes):
             if v_end <= s_start:
                 result.append({**v,
                                "duration": v_end - v_start,
-                               "type": "visual", "cuts": 1.0, "cut_times": v_start})
+                               "type": "visual", "cuts": 1.0, "cut_times": v_start,
+                               "cut_per_sec": vs_count/(s_end - s_start)})
+
                 ind_vs += 1
                 continue
 
@@ -39,7 +41,8 @@ def build_scene_data(visual_scenes, speech_scenes):
                                "duration":   v_end - v_start,
                                "cuts": vs_count,
                                "type": "visual",
-                               "cut_times": cut_times})
+                               "cut_times": cut_times,
+                               "cut_per_sec": vs_count/(s_end - s_start)})
 
             if v_end > s_end:
                 visual_scenes[ind_vs] = {**v, "start_time": s_end}
@@ -86,7 +89,7 @@ def merge_vs(scenes):
     if buf:
         result.append(merge(buf))
 
-    return result
+    return add_scene_id(result)
 
 
 def merge(scenes):
@@ -97,3 +100,9 @@ def merge(scenes):
     cuts = len(scenes)
     return {"start_time": start, "end_time": end, "duration": duration,
             "cuts": cuts, "type": "visual", "cut_per_sec": cuts / duration, "cut_times": cut_times}
+def add_scene_id(scenes):
+
+    return [
+        {"scene_id": i, **sc}
+        for i, sc in enumerate(scenes)
+    ]
